ffedde4c9e928cf7523b7ed9f410c73f21c1b7ec
andreburto <tuglyraisin@gmail.com>
2025-11-08 18:43:54 -0600

    We use https now.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About
 
-The app: http://turboman.mothersect.info/
+The app: https://turboman.mothersect.info/
 
 In December every year I take a picture of my [Turboman](https://www.imdb.com/title/tt0116705/) action figure in front of a picture of my Turboman action figure. This app is a way for me to easily browse through those pictures.
 
--- terraform/turboman.tf
+++ terraform/turboman.tf
@@ -71,8 +71,8 @@ resource "aws_route53_record" "turboman" {
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket.tm_bucket.website_domain
-    zone_id                = aws_s3_bucket.tm_bucket.hosted_zone_id
+    name                   = aws_cloudfront_distribution.turboman.domain_name
+    zone_id                = aws_cloudfront_distribution.turboman.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -95,4 +95,74 @@ resource "aws_s3_bucket_object" "index" {
   key          = each.value
   source       = "${var.base_path}/${var.source_dir}/${each.value}"
   etag         = filemd5("${var.base_path}/${var.source_dir}/${each.value}")
+}
+
+resource "aws_acm_certificate" "turboman" {
+  domain_name       = var.domain_url
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "turboman_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.turboman.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.zone_id
+}
+
+resource "aws_cloudfront_distribution" "turboman" {
+  origin {
+    domain_name              = aws_s3_bucket.tm_bucket.bucket_domain_name
+    origin_id                = aws_s3_bucket.tm_bucket.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  aliases = [var.domain_url]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.tm_bucket.id
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = aws_acm_certificate.turboman.arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
 }
